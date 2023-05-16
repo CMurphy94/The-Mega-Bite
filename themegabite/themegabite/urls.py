6cb155627c90b0a1d8903c1ccc3bcd2fe46e6f5b
CMurphy94 <conormurphy94@hotmail.com>
2023-05-16 20:56:21 +0000

    Add additional Django for URL

--- themegabite/themegabite/urls.py
+++ themegabite/themegabite/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from bookingform.views import make_reservation
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('reservation/', make_reservation, name='reservation'),
 ]
